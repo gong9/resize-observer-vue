1a690d5aa1eb0288ac5640c2cb656bd461d6c402
gong <517441540@qq.com>
2022-12-09 23:31:43 +0800

    docs: hanlde style

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: 'VitePress',
+  title: 'resize-observer-vue',
+  base: '.',
   themeConfig: {
     nav: [
       {
